049d15c4d54b8c70840a9eb910cd346ccb3ff8ad
lucholaf <lucholaf@1a63dafe-9f17-0410-9df7-a0250be266ed>
2006-12-01 06:44:48 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/phpfit/trunk@74 1a63dafe-9f17-0410-9df7-a0250be266ed

--- tests/FixtureTest.php
+++ tests/FixtureTest.php
@@ -5,13 +5,11 @@ require_once 'PHPFIT/Fixture.php';
 class FixtureTest extends UnitTestCase {
 	
 	public function testCamelOneSpace() {
-		$string = "my string";
-		$this->assertEqual("myString", PHPFIT_Fixture::camel($string));
+		$this->assertEqual("myString", PHPFIT_Fixture::camel('my string'));
 	}
 	
 	public function testCamelTwoSpace() {
-		$string = "my string two";
-		$this->assertEqual("myStringTwo", PHPFIT_Fixture::camel($string));
+		$this->assertEqual("myStringTwo", PHPFIT_Fixture::camel('my string two'));
 	}
 }
 
