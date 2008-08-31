ce3f430f5edf8bc25df6bc4913e51cabd1ab29c8
lucholaf <lucholaf@1a63dafe-9f17-0410-9df7-a0250be266ed>
2008-08-31 16:32:49 +0000

    some doc added to classes
    git-svn-id: http://svn.berlios.de/svnroot/repos/phpfit/trunk@88 1a63dafe-9f17-0410-9df7-a0250be266ed

--- PHPFIT/FixtureLoader.php
+++ PHPFIT/FixtureLoader.php
@@ -14,17 +14,26 @@ class PHPFIT_FixtureLoader {
     */
     public static function load($fixtureName, $fixturesDirectory) {
         if (substr($fixtureName, 0, strlen(self::$fitPrefix)) == self::$fitPrefix) {
-            return self::loadFitFixture($fixtureName);
+            return self::loadFitFixture($fixtureName); // if $fixtureName starts with "fit."
         } else {
             return self::loadUserFixture($fixtureName, $fixturesDirectory);
         }
     }
 
+    /**
+    * Example: $fixtureName = My.Path.To.Fixture
+    * Will search for the file My/Path/to/Fixture.php
+    * Then, first will try to find a class named "Fixture"
+    * Otherwise, will try to find a class named "My_Path_To_Fixture" (pear-style)
+    *
+    * @param string $fixtureName
+    * @param string $fixturesDirectory
+    */
     private static function loadUserFixture($fixtureName, $fixturesDirectory) {
         if ($fixturesDirectory != null)
-            $filename = $fixturesDirectory . DIRECTORY_SEPARATOR . str_replace('.', '/', $fixtureName) . '.php';
+            $filename = $fixturesDirectory . DIRECTORY_SEPARATOR . str_replace('.', DIRECTORY_SEPARATOR, $fixtureName) . '.php';
         else
-            $filename = str_replace('.', '/', $fixtureName) . '.php';
+            $filename = str_replace('.', DIRECTORY_SEPARATOR, $fixtureName) . '.php';
 
         self::loadFile($filename);
     
@@ -42,19 +51,29 @@ class PHPFIT_FixtureLoader {
         if (class_exists($pearClassname))
             return new $pearClassname;
             
-        throw new Exception('Class could not be found in file ' . $filename);
+        throw new Exception('Class "'. $commonClassname. '" or "' . $pearClassname . '" could not be found in file ' . $filename);
     }
     
+    /**
+    * Example: $fixtureName = fit.Action and $fitFixturesDirectory = PHPFIT/Fixture
+    * Will search in PHPFIT/Fixture/Action.php
+    * Then, will try to find a class named PHPFIT_Fixture_Action
+    *
+    * @param string $fixtureName
+    */
     private static function loadFitFixture($fixtureName) {
         $fixtureWithoutPrefix = str_replace('fit.', '', $fixtureName);
         
-        $filename = self::$fitFixturesDirectory . DIRECTORY_SEPARATOR . str_replace('.', '/', $fixtureWithoutPrefix) . '.php';
+        $filename = self::$fitFixturesDirectory . DIRECTORY_SEPARATOR . str_replace('.', DIRECTORY_SEPARATOR, $fixtureWithoutPrefix) . '.php';
     
         self::loadFile($filename);
         
-        $classname = str_replace('/', '_', self::$fitFixturesDirectory) . '_' . $fixtureWithoutPrefix;
+        $classname = str_replace(DIRECTORY_SEPARATOR, '_', self::$fitFixturesDirectory) . '_' . $fixtureWithoutPrefix;
         
-        return new $classname;
+        if (class_exists($classname))
+            return new $classname;
+        
+        throw new Exception('Class "' . $classname . '" could not be found in file ' . $filename);
     }
     
     private static function loadFile($filename) {
