5ffe8794dfe78243088d0284cd6bad22f38e4cf1
ggramlich <ggramlich@1a63dafe-9f17-0410-9df7-a0250be266ed>
2009-02-05 16:06:09 +0000

    Ant build file for testing and phpcs
    git-svn-id: http://svn.berlios.de/svnroot/repos/phpfit/trunk@131 1a63dafe-9f17-0410-9df7-a0250be266ed

--- tests/AllTest.php
+++ tests/AllTest.php
@@ -15,9 +15,12 @@ $test->addTestFile('tests/FileRunnerTest.php');
 $test->addTestFile('tests/FrameworkTest.php');
 
 if (TextReporter::inCli()) {
-    $test->run(new TextReporter());
+    $success = $test->run(new TextReporter());
 } else {
-    $test->run(new HtmlReporter());
+    $success = $test->run(new HtmlReporter());
 }
-
+if (!$success) {
+    // Exit with error code to make the ant build fail.
+    exit(1);
+}
 ?>
